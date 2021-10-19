1aa71ea2a3b7a18e0f9c2227813b2123a4e20557
Keisuke Fujii <keisuke198619@gmail.com>
2021-10-19 10:15:34 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-cd /home/fujii/workspace4/work/ABM
-source $HOME/workspace4/virtualenvs/cause/bin/activate 
 
 # bats
 # python run_grid_search.py --experiment bats --model gvar --K 3 --self_other --CF_pred --percept --test_samples 1 --TEST 
@@ -21,19 +19,19 @@ source $HOME/workspace4/virtualenvs/cause/bin/activate
 
 # create kuramoto dataset: the length is a finally obtained length 
 # cd datasets
-# python generate_ODE_dataset --num_train 64 --num_valid 64 --num_test 64 --sample_freq 40 --length 200 --length_test 200 
+# python generate_ODE_dataset.py --num_train 64 --num_valid 64 --num_test 64 --sample_freq 40 --length 200 --length_test 200 
 
 # create boid dataset: 
-# python -m generate_boid_dataset --num-train 10 --num-valid 10 --num-test 10 --sample_freq 100 --length 200 --length_test 200 --bat --partial --avoid #--video 10 
+# python generate_boid_dataset.py --num-train 10 --num-valid 10 --num-test 10 --sample_freq 100 --length 200 --length_test 200 --bat --partial --avoid #--video 10 
 
 # kuramoto (VAR, GVAR/+self_other/+CF_pred)
-python run_grid_search.py --experiment kuramoto --model gvar --K 5 --TEST
+# python run_grid_search.py --experiment kuramoto --model gvar --K 5 --TEST
 # python run_grid_search.py --experiment kuramoto --model gvar --K 5 --self_other --TEST
-python run_grid_search.py --experiment kuramoto --model gvar --K 5 --self_other --CF_pred --TEST # 
+# python run_grid_search.py --experiment kuramoto --model gvar --K 5 --self_other --CF_pred --TEST # 
 
 # boid (GVAR/+self_other/+CF_pred/+percept)
-python run_grid_search.py --experiment boid5_bat --model gvar --K 3 --TEST
+# python run_grid_search.py --experiment boid5_bat --model gvar --K 3 --TEST
 # python run_grid_search.py --experiment boid5_bat --model gvar --K 3 --self_other
 # python run_grid_search.py --experiment boid5_bat --model gvar --K 3 --self_other --CF_pred 
 # python run_grid_search.py --experiment boid5_bat --model gvar --K 3 --self_other --percept 
-python run_grid_search.py --experiment boid5_bat --model gvar --K 3 --self_other --CF_pred --percept --TEST #
+# python run_grid_search.py --experiment boid5_bat --model gvar --K 3 --self_other --CF_pred --percept --TEST #
